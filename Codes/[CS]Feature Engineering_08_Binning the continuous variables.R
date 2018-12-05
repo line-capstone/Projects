e7e21e35f30a1da052f0ceb4f0d691bd057a1670
mangodm <dmhuh1003@gmail.com>
2018-12-06 06:08:39 +0900

    Add [CS] Feature Engineering 08 file

---
+++ Codes/[CS]Feature Engineering_08_Binning the continuous variables.R
@@ -0,0 +1,58 @@
+## -- Outbrain Click Prediction 연속형 변수 Quantile 구하기
+
+# 경로 설정
+setwd('F:/Outbrain Click Prediction')
+
+# 필요한 패키지 로드 
+library(data.table)
+library(OneR)
+
+# 데이터 로드
+train <- fread('calculating_continuous.csv') 
+
+# 변수명 확인
+names(train)
+
+# 변수별 Q1(25%), Q2(50%), Q3(75%) 값 구하기
+
+## -- pop_ad_id
+# Q1: 0.094, Q2: 0.169, Q3: 0.266
+quantile(train$pop_ad_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_ad_doc_id
+# Q1: 0.098, Q2: 0.170, Q3: 0.262
+quantile(train$pop_ad_doc_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_ad_publisher_id
+# Q1: 0.111, Q2: 0.178, Q3: 0.257
+quantile(train$pop_ad_publisher_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_ad_advertiser_id
+# Q1: 0.109, Q2: 0.177, Q3: 0.258
+quantile(train$pop_advertiser_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_campaign_id
+# Q1: 0.098, Q2: 0.174, Q3: 0.258
+quantile(train$pop_campaign_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_view_doc_ad_doc
+# Q1: 0.067, Q2: 0.154, Q3: 0.290
+quantile(train$pop_view_doc_ad_doc, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_ad_source_id
+# Q1: 0.111, Q2: 0.177, Q3: 0.256
+quantile(train$pop_ad_source_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_ad_topic_id
+# Q1: 0.177, Q2: 0.199, Q3: 0.217
+quantile(train$pop_ad_topic_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+## -- pop_ad_category_id
+# Q1: 0.168, Q2: 0.204, Q3: 0.227
+quantile(train$pop_ad_category_id, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+# OneR 패키지 사용하여 binning 
+train_bin <- bin(train, nbins = 4, method = 'content', na.omit = FALSE,
+                 labels = c('1', '2', '3', '4'))
+
+write_csv(train_bin, 'train_bin.csv')
